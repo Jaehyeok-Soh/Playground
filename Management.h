9749e18d2b9eff836e1b515676ddb17ee0d26436
Jaehyeok-Soh <revol3154@gmail.com>
2025-04-09 14:25:25 +0900

    check memory leak

--- Management.h
+++ Management.h
@@ -3,19 +3,6 @@
 #include <iostream>
 #include "CharactorBase.h"
 
-#ifdef _DEBUG
-
-#define _CRTDBG_MAP_ALLOC
-#include <stdlib.h>
-#include <crtdbg.h>
-
-#ifndef DBG_NEW 
-#define DBG_NEW new ( _NORMAL_BLOCK , __FILE__ , __LINE__ ) 
-#define new DBG_NEW 
-
-#endif
-#endif
-
 using namespace std;
 using json = nlohmann::json;
 
